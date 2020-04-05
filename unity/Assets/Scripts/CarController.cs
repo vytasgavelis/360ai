24fcb0725d493f46f802a0b2140c4192f0ccd620
Vytas <gavelisvytas@gmail.com>
2020-04-05 09:15:42 -0700

    vehicle movement and raycasting

---
+++ unity/Assets/Scripts/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class CarController : MonoBehaviour
+{
+    public InputManager im;
+    public RaycastManager rm;
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+
+    public float strengthCoefficient = 90000f;
+    public float steerCoefficientMultiplier = 1.5f;
+
+    public float targetDistance;
+
+    private void Start()
+    {
+        im = GetComponent<InputManager>();
+    }
+
+    void FixedUpdate()
+    {
+        leftWheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
+        rightWheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
+
+        if (im.steer != 0)
+        {
+            leftWheel.motorTorque = im.steer * steerCoefficientMultiplier * strengthCoefficient * Time.deltaTime;
+            rightWheel.motorTorque = -im.steer * steerCoefficientMultiplier * strengthCoefficient * Time.deltaTime;
+        }
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(rm.leftDistance + " " + rm.rightDistance + " " + rm.forwardDistance + " " + rm.backwardsDistance);
+    }
+}
---
+++ unity/Assets/Scripts/RaycastManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastManager : MonoBehaviour
+{
+    public float forwardDistance;
+    public float backwardsDistance;
+    public float leftDistance;
+    public float rightDistance;
+
+    public float avoidDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        Vector3 backwards = transform.TransformDirection(Vector3.back) * 10;
+        Debug.DrawRay(transform.position, backwards, Color.green);
+
+        Vector3 left = transform.TransformDirection(Vector3.left) * 10;
+        Debug.DrawRay(transform.position, left, Color.green);
+
+        Vector3 right = transform.TransformDirection(Vector3.right) * 10;
+        Debug.DrawRay(transform.position, right, Color.green);
+
+        forwardDistance = 0;
+        backwardsDistance = 0;
+        rightDistance = 0;
+        leftDistance = 0;
+
+        if (Physics.Raycast(transform.position, forward, out hit))
+        {
+            forwardDistance = hit.distance;
+        }
+        if (Physics.Raycast(transform.position, backwards, out hit))
+        {
+            backwardsDistance = hit.distance;
+        }
+        if (Physics.Raycast(transform.position, left, out hit))
+        {
+            leftDistance = hit.distance;
+        }
+        if (Physics.Raycast(transform.position, right, out hit))
+        {
+            rightDistance = hit.distance;
+        }
+
+
+        DrawRays();
+    }
+
+    void DrawRays()
+    { 
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        Vector3 backwards = transform.TransformDirection(Vector3.back) * 10;
+        Debug.DrawRay(transform.position, backwards, Color.green);
+
+        Vector3 left = transform.TransformDirection(Vector3.left) * 10;
+        Debug.DrawRay(transform.position, left, Color.green);
+
+        Vector3 right = transform.TransformDirection(Vector3.right) * 10;
+        Debug.DrawRay(transform.position, right, Color.green);
+    }
+}
