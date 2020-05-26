70c30dff2db36576da537bd4e06a4fe6501e8512
Vytas Gavelis <gavelisvytas@gmail.com>
2020-05-25 19:27:05 -0700

    Added additional rotation after detecting obstacle

--- arduino/sketch_apr12a.ino
+++ arduino/sketch_apr12a.ino
@@ -1,69 +1,356 @@
-#include <Arduino.h>
+//include <Arduino.h>
 
-class AFDCP {
-  private:
-      int a;
-  public:
-    AFDCP() {
-      
-      pinMode(7, OUTPUT);
-      pinMode(8, OUTPUT);
-      pinMode(9, OUTPUT);
+//==================================
+//=VARIABLES========================
+//==================================
+int echo_front = 4;
+int echo_right = 10;
+int echo_left = 2;
+
+int trig_front = 3;
+int trig_right = 11;
+int trig_left = 5;
+
+int MOVE_FORWARDS = 1;
+int MOVE_BACKWARDS = 2;
+int TURN_LEFT = 3;
+int TURN_RIGHT = 4;
+int WAIT = 5;
+int STOP = 6;
+int PANIC = 7;
+int BACK = 8;
+int State;
+
+//public float CarSpeed = 2f;
+//public float RotateSpeed = 2f;
+int CriticalDistance = 15;
+int CriticalDistanceSide = 20;        // This var is experimental. Might not be needed in the future.
+int CriticalDistanceWhenTurning = 10; // This should be about 20-30% larger than CriticalDistance in order to eliminate laggy rotation.
+int SecondsToPanic = 5;
+bool IsApproachingLeft;
+bool IsApproachingRight;
+int LastForwardCommand;
+
+int RightDistanceHistory[5];
+int LeftDistanceHistory[5];
+int hisLen = 5;
+
+//==================================
+//==================================
+//==================================
+
+class InputManager
+{
+private:
+public:
+  int upDistance;
+  int leftDistance;
+  int rightDistance;
+  int HistoryCounter = 0;
+
+  //public float rayDistance = 50;
+
+  //public LayerMask ObstacleLayerMask;
+
+  InputManager()
+  {
+    for (int i = 0; i < hisLen; i++)
+    {
+      RightDistanceHistory[i] = 100;
+      LeftDistanceHistory[i] = 100;
     }
-  
-    void STOP() {
-       digitalWrite(7, LOW);
-       digitalWrite(8, LOW);
-       digitalWrite(9, LOW);
+  }
+
+  void FixedUpdate()
+  {
+    RaycastCheckUpdate();
+  }
+
+  int dist(int echoPin, int trigPin)
+  {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    double duration = pulseIn(echoPin, HIGH);
+
+    double distance = duration * 0.034 / 2;
+
+    //Serial.print("Distance: ");
+    //Serial.println(distance);
+
+    return distance;
+  }
+
+  void RaycastCheckUpdate()
+  {
+    upDistance = dist(echo_front, trig_front);
+    leftDistance = dist(echo_left, trig_left);
+    rightDistance = dist(echo_right, trig_right);
+
+    // Keep track of previous distances.
+    RightDistanceHistory[HistoryCounter] = rightDistance;
+    LeftDistanceHistory[HistoryCounter] = leftDistance;
+    HistoryCounter++;
+
+    if (HistoryCounter >= 5)
+    {
+      HistoryCounter = 0;
     }
-    
-    void GO() {
-       digitalWrite(7, LOW);
-       digitalWrite(8, LOW);
-       digitalWrite(9, HIGH);
-    }
-    
-    void BACK() {
-       digitalWrite(7, LOW);
-       digitalWrite(8, HIGH);
-       digitalWrite(9, LOW);
-    }
-    
-    void LEFT() {
-       digitalWrite(7, LOW);
-       digitalWrite(8, HIGH);
-       digitalWrite(9, HIGH);
-    }
-    
-    void RIGHT() {
-       digitalWrite(7, HIGH);
-       digitalWrite(8, LOW);
-       digitalWrite(9, LOW);
-    }
-    
+
+    //Debug.Log(upDistance + " " + leftDistance + " " + rightDistance);
+  }
+};
+
+class AFDCP
+{
+private:
+  int a;
+
+public:
+  AFDCP()
+  {
+
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+  }
+
+  void STOP()
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+  }
+
+  void GO()
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, HIGH);
+  }
+
+  void BACK()
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+  }
+
+  void LEFT()
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+  }
+
+  void RIGHT()
+  {
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+  }
 };
 
 //CLASSES
 AFDCP car;
+InputManager im;
 // AFDCP - Arduino & FEZ Digital Communication Protocol
 // ----------------------------------------
 
-void setup() {
+void setup()
+{
+  car.STOP();
+
+  //-----------------------
+  pinMode(trig_front, OUTPUT);
+  pinMode(echo_front, INPUT);
+  //-----------------------
+  pinMode(trig_right, OUTPUT);
+  pinMode(echo_right, INPUT);
+  //-----------------------
+  pinMode(trig_left, OUTPUT);
+  pinMode(echo_left, INPUT);
+  //-----------------------
+
+  State = MOVE_FORWARDS;
+  LastForwardCommand = 0;
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  im.FixedUpdate();
+  HandleStates();
+  IsApproachingLeft = IsApproaching(LeftDistanceHistory);
+  IsApproachingRight = IsApproaching(RightDistanceHistory);
+  /*if (LastForwardCommand <= Time.realtimeSinceStartup - SecondsToPanic) // You should replace this with micros() function in arduino.
+  {
+    State = PANIC;
+  }*/
+
+  delay(1);
+}
+
+void Waiting()
+{
+  if (im.upDistance >= CriticalDistance && !IsApproachingRight && !IsApproachingLeft)
+  {
+    State = MOVE_FORWARDS;
+  }
+  else if (IsApproachingRight)
+  {
+    State = TURN_LEFT;
+  }
+  else if (IsApproachingLeft)
+  {
+    State = TURN_RIGHT;
+  }
+  else if (im.upDistance <= CriticalDistance)
+  {
+    
+    State = BACK;
+  }
+  else
+  {
+    State = PANIC;
+  }
+}
+
+void MoveForwards()
+{
+  //LastForwardCommand = Time.realtimeSinceStartup; // Replace with micros() function.
+  if (im.upDistance <= CriticalDistance || IsApproachingRight || IsApproachingLeft)
+  {
+    State = WAIT;
+  }
+
+  car.GO(); // Move vehicle forwards
+}
+
+void MoveBackwards()
+{
+  car.BACK(); // Move vehicle backwards
+  delay(1000);
+  im.FixedUpdate();
+  if (im.leftDistance >= im.rightDistance)
+  {
+      car.LEFT();
+      delay(1000);
+  }
+  else
+  {
+      car.RIGHT();
+      delay(1000);
+  }
+  State = MOVE_FORWARDS;
+  //State = im.leftDistance >= im.rightDistance ? TURN_LEFT : TURN_RIGHT;
+}
+
+void TurnLeft()
+{
+  if (im.rightDistance >= CriticalDistanceSide && im.upDistance >= CriticalDistanceWhenTurning)
+  {
+    State = WAIT;
+  }
+  car.LEFT(); // Turn vehicle left
+}
+
+void TurnRight()
+{
+  if (im.leftDistance >= CriticalDistanceSide && im.upDistance >= CriticalDistanceWhenTurning)
+  {
+    State = WAIT;
+  }
+  car.RIGHT(); // Turn vehicle right
+}
+
+void Stop()
+{
   car.STOP();
 }
 
-void loop() {
-  delay(3000);
-  
-  car.GO();
-  delay(3000);
-
+void Panic()
+{
   car.STOP();
-  delay(3000);
-  
-  car.RIGHT();
-  delay(2000);
-
-    car.STOP();
-
+  // Make vehicle play beeping sound.
+  // Vehicle now shall be controlled by a remote controller.
+  // Restore STATE to WAIT after user is done controlling the vehicle.
 }
+
+void HandleStates()
+{
+  switch (State)
+  {
+  case 1:
+    MoveForwards();
+    break;
+  case 2:
+    MoveBackwards();
+    break;
+  case 3:
+    TurnLeft();
+    break;
+  case 4:
+    TurnRight();
+    break;
+  case 5:
+    Waiting();
+    break;
+  case 7:
+    Panic();
+    break;
+  case 8:
+    MoveBackwards();
+    break;
+  default:
+    Stop();
+    break;
+  }
+}
+
+bool IsApproaching(int *DistanceHistory)
+{
+  // You can find distance history implementation in InputManager.cs line 70.
+  // DistanceHistory array may be made longer if lack of sensor precision is creating any issues.
+  bool IsApproaching = false;
+  int SmallerDistanceCounter = 0;
+  for (int i = 1; i < hisLen; i++)
+  {
+    if (DistanceHistory[i] <= CriticalDistanceSide && DistanceHistory[i] < DistanceHistory[i - 1])
+    {
+      SmallerDistanceCounter++;
+    }
+  }
+  if (SmallerDistanceCounter >= hisLen / 2)
+  {
+    IsApproaching = true;
+  }
+
+  return IsApproaching;
+}
+
+/*
+int dist(int echoPin, int trigPin)
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  double duration = pulseIn(echoPin, HIGH);
+  
+  double distance= duration*0.034/2;
+  
+  //Serial.print("Distance: ");
+  //Serial.println(distance);
+
+  return distance;
+}
+*/
